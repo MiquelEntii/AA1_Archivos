71430620bb091194023e5c15666fbc2b9e4d3de9
MiquelEntii <miquel.villar@enti.cat>
2024-05-20 18:13:07 +0200

    Codigo comentado

--- Archivos/main.cpp
+++ Archivos/main.cpp
@@ -32,38 +32,45 @@ void main() {
 		while (getline(file_read, linea)) {
 			empty++;
 		}
-		//Reinicio el puntero al principio del archivo
+		//Reinicio el puntero al principio del archivo ya que ahora despues de leer las lineas esta al final
 		file_read.clear();
 		file_read.seekg(0);
 
-		
+		//Si empty es 0 significa que el archivo esta vacío
 		if (empty == 0) {
 			std::cout << "No se han encontrado mensajes nuevos" << std::endl;
 
 			std::cout << "Escribe tantos mensajes como quieras, (esribe 'exit' cuando termines)" << std::endl;
 			//Bucle para que el usuario pueda escribir hasta que escriba exit
 			do {
-
+				//Input del user
 				std::cin >> linea;
 
 
 				//Hago un if por si la palabra es exit que no se escriba en el archivo
 				if (linea != "exit") {
+					//Le paso el input a otra variable la cual voy a encriptar
 					std::string lineaEncriptada = linea;
 
+					//Aqui hago un bucle for each que va muy bien en esta situación ya que itera por cada char que haya dentro de la palabra
 					for (char c : lineaEncriptada) {
+						//El char lo convierte en -5 de la tabla ascii
 						c = c - 5;
+						//Escribo ese char dentro del archivo
 						file_input << c;
+						//Voy calculando el checksum
 						checksum = checksum + c;
 					}
+					//Despues de cada palabra hago un salto de linea
 					file_input << std::endl;
 
 				}
+				//Este if esta para que se escriba el checksum una vez el usuario haya escrito exit, el checksum no esta en la cabecera porque no he sabido hacerlo correctamente
 				if (linea == "exit") {
 					file_input << checksum;
 				}
 
-
+				//Cuando el input es exit nos salimos del archivo
 			} while (linea != "exit");
 
 
@@ -81,9 +88,12 @@ void main() {
 				std::cin >> recuperar;
 			} while (recuperar != 's' && recuperar != 'n');
 
+			//Si quiere recuperar los mensajes se metera en este condicional
 			if (recuperar == 's') {
 
+				//Esta parte es la que uso para desencriptar los mensajes
 				while (getline(file_read, linea)) {
+					//El bucle for each es exactamente igual pero en vez de restar 5 suma esos 5 y lo muestra en pantalla
 					for (char c : linea) {
 						c = c + 5;
 						std::cout << c;
@@ -122,9 +132,11 @@ void main() {
 
 			else {
 
+				//Cierro los archivos ya que si hemos entrado en este else debemos abrir el archivo en truncal para borrar todo lo anterior
 				file_read.close();
 				file_input.close();
 
+				//Los abro en modo truncal
 				file_read.open("elemento.txt", std::ios::trunc);
 				file_input.open("elemento.txt", std::ios::trunc);
 
@@ -162,6 +174,7 @@ void main() {
 
 	}
 
+	//Cierro los archivos para acabar
 	file_read.close();
 	file_input.close();
 	
